3d160078ad55bf49fb998a17abf4ae6f29d47d68
Pratik <pratikchakraborty2020@gmail.com>
2023-04-04 21:19:24 +0530

    Added animation for date/week/month value in home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,13 +33,48 @@ export class HomeComponent {
   amount: number[] = [360, 80];
   admin: string = "Sharuk";
   flatmate: string[] = ["Gaurav", "Pratik"];
-
+  viewBy: string = "1";
+  value: string = "";
   constructor(public router: Router) { }
-
+  ngOnInit() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    this.value = mm + '/' + dd + '/' + yyyy;
+  }
   toOldStatsPage() {
     this.router.navigate(['old-stats']);
   }
   toLogsPage() {
     this.router.navigate(['logs']);
+  }
+
+  updateValue() {
+    if (this.viewBy == "1") {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0');
+      var yyyy = today.getFullYear();
+      this.value = mm + '/' + dd + '/' + yyyy;
+    }
+    else if (this.viewBy == "2") {
+      var date1 = new Date();
+      var date2 = new Date();
+      var months = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+        "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+      date1.setDate(date1.getDate() - date1.getDay());
+      date2.setDate(date2.getDate() - date2.getDay());
+      var startDayOfWeek = date1;
+      date2.setDate(date2.getDate() + 7);
+      var endDayOfWeek = date2;
+      this.value = startDayOfWeek.getDate().toString() + " " + months[startDayOfWeek.getMonth()] + " - " + endDayOfWeek.getDate().toString() + " " + months[endDayOfWeek.getMonth()];
+    }
+    else if (this.viewBy == "3") {
+      var months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+      var today = new Date();
+      this.value = months[today.getMonth()];
+    }
   }
 }
