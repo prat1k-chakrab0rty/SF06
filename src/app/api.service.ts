e164a1a258ebe3b29b815ef5d361028f513b5b77
pratik.c <pratik.c@FWS-LP-1019>
2023-04-06 21:27:05 +0530

    Added api call for login.

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseApiUrl:string="https://light-gold-chipmunk.cyclic.app"
+  constructor(private http: HttpClient) { }
+
+  login(passcode:string): Observable<any> {
+    return this.http.get<any>(this.baseApiUrl + '/users/'+passcode);
+  }
+}
