1f02f305ff939a50a92f95d84c07c2210bb171ad
pratik.c <pratik.c@FWS-LP-1019>
2023-04-13 23:54:48 +0530

    Fixed the URL.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  baseApiUrl: string = "http://light-gold-chipmunk.cyclic.app";
+  baseApiUrl: string = "https://light-gold-chipmunk.cyclic.app";
   constructor(private http: HttpClient) { }
 
   login(passcode: string): Observable<any> {
